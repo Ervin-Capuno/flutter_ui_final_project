5715ffe2e110dc4c8fd4b806710e7bcf5e89bdab
Ervin-Capuno <ervinreycapuno23@gmail.com>
2024-07-25 14:55:03 +0800

    Updated the paths fo the images

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -4,20 +4,20 @@ import '../models/devices_model.dart';
 List<Room> rooms = [
   Room(
     name: 'Living Room',
-    backgroundImage: 'img/living_room.jpg',
+    backgroundImage: 'img/app_assets/living_room.jpg',
     temperature: 31.0,
     lightLevel: 89.1,
     control: 'Automatic',
     devices: [
       Devices(
         name: 'TV',
-        image: 'img/tv.png',
+        image: 'img/app_assets/tv.png',
         state: 'On',
         consumption: '150 kWh',
       ),
       Devices(
         name: 'Lamp',
-        image: 'img/lamp.jpg',
+        image: 'img/app_assets/lamp.jpg',
         state: 'Off',
         consumption: '20 kWh',
       ),
@@ -25,14 +25,14 @@ List<Room> rooms = [
   ),
   Room(
     name: 'Kitchen',
-    backgroundImage: 'img/kitchen.jpg',
+    backgroundImage: 'img/app_assets/kitchen.jpg',
     temperature: 27.0,
     lightLevel: 87.1,
     control: 'Manual',
     devices: [
       Devices(
         name: 'Electric Stove',
-        image: 'img/electric_stove.jpg',
+        image: 'img/app_assets/electric_stove.jpg',
         state: 'Off',
         consumption: '31 kWh',
       ),
